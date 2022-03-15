14f80d510481237bd55b028bea60f964ff14460e
Beatriz <beatriz.matos@dtidigital.com.br>
2022-03-15 16:17:05 -0300

    creating test for empty stack

---
+++ src/main/java/entity/Pilha.java
@@ -0,0 +1,15 @@
+package entity;
+
+public class Pilha {
+
+    public boolean estaVazia() {
+        return true;
+    }
+
+    public int tamanho() {
+        return 0;
+    }
+
+    public void empilha(String primeiro) {
+    }
+}
---
+++ src/test/java/TestePilha.java
@@ -0,0 +1,15 @@
+import entity.Pilha;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class TestePilha {
+
+    @Test
+    public void pilhaVazia() {
+        Pilha p = new Pilha();
+        assertTrue(p.estaVazia());
+        assertEquals(0, p.tamanho());
+    }
+}
