92f33f9436f46500fa823fc1a2411ba859f8f9a8
Beatriz <beatriz.matos@dtidigital.com.br>
2022-03-28 06:03:37 -0300

    criando testes unitários para classe pilha

--- src/main/java/entity/Pilha.java
+++ src/main/java/entity/Pilha.java
@@ -2,14 +2,29 @@ package entity;
 
 public class Pilha {
 
+    private Object[] elementos = new Object[10];
+    private int quantidade = 0;
+
     public boolean estaVazia() {
-        return true;
+        return quantidade == 0;
     }
 
     public int tamanho() {
-        return 0;
+        return quantidade;
     }
 
-    public void empilha(String primeiro) {
+    public void empilha(Object elemento) {
+        this.elementos[quantidade] = elemento;
+        quantidade++;
+    }
+
+    public Object topo() {
+        return elementos[quantidade - 1];
+    }
+
+    public Object desempilha() {
+        Object topo = topo();
+        quantidade--;
+        return topo;
     }
 }
--- src/test/java/TestePilha.java
+++ src/test/java/TestePilha.java
@@ -12,4 +12,27 @@ public class TestePilha {
         assertTrue(p.estaVazia());
         assertEquals(0, p.tamanho());
     }
+
+    @Test
+    public void empilhaUmElemento() {
+        Pilha p = new Pilha();
+        p.empilha("primeiro");
+        assertFalse(p.estaVazia());
+        assertEquals(1, p.tamanho());
+        assertEquals("primeiro", p.topo());
+    }
+
+    @Test
+    public void empilhaEDesempilha() {
+        Pilha p = new Pilha();
+        p.empilha("primeiro");
+        p.empilha("segundo");
+        assertEquals(2, p.tamanho());
+        assertEquals("segundo", p.topo());
+
+        Object desempilhado = p.desempilha();
+        assertEquals(1, p.tamanho());
+        assertEquals("primeiro", p.topo());
+        assertEquals("segundo", desempilhado);
+    }
 }
